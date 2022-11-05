0b668b77f8a9b6e49db4b20eefdd41373a17e513
Flor <flor.zattera@gmail.com>
2022-11-05 12:28:56 -0300

    Agregando archivos

---
+++ src/main/java/org/ada/springejemplo/SpringEjemploApplication.java
@@ -0,0 +1,13 @@
+package org.ada.springejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEjemploApplication.class, args);
+	}
+
+}
